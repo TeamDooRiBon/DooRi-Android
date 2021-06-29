c3173f30db39ecee437329d0d558f91e6c14efba
SSong-develop <thdgnsrl123@naver.com>
2021-06-30 01:33:42 +0900

    [FEAT] : DooriBonRoundShadowImageView 만듬

---
+++ app/src/main/java/kr/co/dooribon/utils/ContextExtension.kt
@@ -0,0 +1,13 @@
+package kr.co.dooribon.utils
+
+import android.content.Context
+import kotlin.math.roundToInt
+
+/**
+ * Created by SSong-develop 2021-06-29
+ *
+ * Just use For CustomView dp , Not Whole Projects
+ */
+fun Context.dpToPixel(dp : Int) : Int = (dp * resources.displayMetrics.density).roundToInt()
+
+fun Context.dpToPixelFloat(dp : Int) : Float = (dp * resources.displayMetrics.density).roundToInt().toFloat()
---
+++ app/src/main/java/kr/co/dooribon/view/DooRiBonRoundShadowImageView.kt
@@ -0,0 +1,87 @@
+package kr.co.dooribon.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import androidx.core.content.res.use
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.ShapeAppearanceModel
+import kr.co.dooribon.R
+import kr.co.dooribon.utils.OnChangeProp
+
+/**
+ * Created by SSong-develop on 2021-06-29
+ *
+ * DooRiBonRoundGradientImageView
+ *
+ * 타원형의 원을 그리지 않을 것이기 때문에 굳이 radius는 그냥 width로 계산함
+ *
+ * app:image_view_alpha , 이미지 위에 색상을 끼얹고 싶을때 이미지가 얼마만큼 보이게 할지? 0.6~0.7이 적당함
+ * app:image_view_shadow_color , 이미지 위에 끼얹고 싶은 색상 설정
+ */
+class DooRiBonRoundShadowImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : ShapeableImageView(context, attrs) {
+
+    private val gradientPaint = Paint()
+
+    private var useWidth = 0
+    private var useHeight = 0
+
+    var shadowColor by OnChangeProp(Color.BLACK) {
+        update()
+    }
+
+    var imageViewAlpha by OnChangeProp(0.7f) {
+        update()
+    }
+
+    init {
+        if (attrs != null) {
+            getStyleableAttrs(attrs)
+        }
+    }
+
+    private fun getStyleableAttrs(attrs: AttributeSet) {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.DooRiBonRoundGradientImageView,
+            0,
+            0
+        ).use {
+            shadowColor = it.getColor(
+                R.styleable.DooRiBonRoundGradientImageView_image_view_shadow_color,
+                shadowColor
+            )
+            imageViewAlpha = it.getFloat(
+                R.styleable.DooRiBonRoundGradientImageView_image_view_alpha,
+                imageViewAlpha
+            )
+        }
+    }
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        update()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawCircle(useWidth / 2f, useHeight / 2f, useWidth / 2f, gradientPaint)
+    }
+
+    private fun update() {
+        useWidth = width
+        useHeight = height
+
+        gradientPaint.apply {
+            color = shadowColor
+            alpha = (imageViewAlpha * 255).toInt()
+        }
+        invalidate()
+    }
+
+}
