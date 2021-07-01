627744920537138e49c137da6f94300ed8bdbef9
Lee WonJoong <realjoong@gmail.com>
2021-07-01 10:38:44 +0900

    [FEAT] #4 - 추천 사진 RecyclerView Data 연결

--- app/src/main/java/kr/co/dooribon/ui/newtrip/AddTravelActivity.kt
+++ app/src/main/java/kr/co/dooribon/ui/newtrip/AddTravelActivity.kt
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import kr.co.dooribon.R
 import kr.co.dooribon.databinding.ActivityNewTravelBinding
+import kr.co.dooribon.ui.newtrip.adapter.ImageData
+import kr.co.dooribon.ui.newtrip.adapter.RecoImgAdapter
 
 class AddTravelActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityNewTravelBinding
+    val tempImgs = mutableListOf<ImageData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,23 @@ class AddTravelActivity : AppCompatActivity() {
         binding.btStartNewTravel.setOnClickListener {
             val intent = Intent(this, DatePickActivity::class.java)
             startActivity(intent)
+        }
+
+        addData()
+        setImgAdapter(tempImgs)
+    }
+
+    private fun setImgAdapter(imgList : List<ImageData>){
+        val imgAdapter = RecoImgAdapter()
+        val imgRV = binding.rvPreparedPhotos
+        imgRV.adapter = imgAdapter
+        imgRV.setHasFixedSize(true) // scroll view 적용 여부에 따라 변경
+        imgAdapter.setItemList(imgList)
+    }
+
+    private fun addData(){
+        for(i in 0 until 16){
+            tempImgs.add(ImageData(R.drawable.dummy_round_view))
         }
     }
 }
--- app/src/main/java/kr/co/dooribon/ui/newtrip/adapter/RecoImgAdapter.kt
+++ app/src/main/java/kr/co/dooribon/ui/newtrip/adapter/RecoImgAdapter.kt
@@ -5,22 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kr.co.dooribon.R
+import kr.co.dooribon.databinding.ItemRecommendedPhotosBinding
 
-class RecoImgAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecoImgAdapter : RecyclerView.Adapter<RecoImgAdapter.ImgViewHolder>() {
 
     private var imgs = mutableListOf<ImageData>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImgViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ItemRecommendedPhotosBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.item_recommended_photos, parent, false)
+        return ImgViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ImgViewHolder, position: Int) {
+        holder.bind(imgs[position])
     }
 
     override fun getItemCount(): Int = imgs.size
+
+    fun setItemList(newList : List<ImageData>){
+        imgs.clear()
+        imgs.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+    class ImgViewHolder(private val binding: ItemRecommendedPhotosBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(img: ImageData) {
+            Glide
+                .with(binding.ivRecoImage)
+                .load(img.img)
+                .centerCrop()
+                .into(binding.ivRecoImage)
+        }
+    }
 
 }
 
