554ad3f513e9fa0863baf65799fed2f8311f03c1
Lee WonJoong <realjoong@gmail.com>
2021-07-01 17:26:20 +0900

    [FEAT] #4 - 여행 추가 view 구현

---
+++ app/src/main/java/kr/co/dooribon/ui/newtrip/TravelPlanDoneActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.dooribon.ui.newtrip
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import kr.co.dooribon.R
+import kr.co.dooribon.databinding.ActivityTravelPlanDoneBinding
+import kr.co.dooribon.ui.home.HomeActivity
+
+class TravelPlanDoneActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityTravelPlanDoneBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_travel_plan_done)
+
+        binding.btnLater.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
+
+    }
+}
