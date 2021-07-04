dabad9cfc51cce0e0a582ac86402a46f35325342
Lee WonJoong <realjoong@gmail.com>
2021-07-04 09:29:48 +0900

    [FEAT] #4 - 새로운 여행 dialog 추가
    - indicator animation은 아직 미완성

--- app/src/main/java/kr/co/dooribon/ui/newtrip/TravelPlanDoneActivity.kt
+++ app/src/main/java/kr/co/dooribon/ui/newtrip/TravelPlanDoneActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import kr.co.dooribon.R
 import kr.co.dooribon.databinding.ActivityTravelPlanDoneBinding
 import kr.co.dooribon.ui.home.HomeActivity
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 class TravelPlanDoneActivity : AppCompatActivity() {
 
@@ -25,14 +28,48 @@ class TravelPlanDoneActivity : AppCompatActivity() {
 
         binding.btnCopyCodes.setOnClickListener {
             val dlg = DoneCopyDialog(this)
-            dlg.start("temp")
+            dlg.start()
+            fixedRateTimer("Change Indicator", false, 0L, 60*1000 ){
 
+            }
             Handler(Looper.getMainLooper()).postDelayed({
+                //changeIndicator()
                 val intent = Intent(this, HomeActivity::class.java)
+                finish()
                 startActivity(intent)
             }, 3000 )
+
         }
+    }
 
+    private fun changeIndicator(){
+        var curIndi = 0
+        val timer = Timer()
+        val indi1 = findViewById<ImageView>(R.id.iv_indi_1)
+        val indi2 = findViewById<ImageView>(R.id.iv_indi_2)
+        val indi3 = findViewById<ImageView>(R.id.iv_indi_3)
 
+        timer.scheduleAtFixedRate(object: TimerTask(){
+            override fun run() {
+                when(curIndi%3){
+                    0->{
+                        indi1.setBackgroundResource(R.drawable.shape_selected_blue_circle)
+                        indi2.setBackgroundResource(R.drawable.shape_unselected_blue_circle)
+                        indi3.setBackgroundResource(R.drawable.shape_unselected_blue_circle)
+                    }
+                    1->{
+                        indi1.setBackgroundResource(R.drawable.shape_unselected_blue_circle)
+                        indi2.setBackgroundResource(R.drawable.shape_selected_blue_circle)
+                        indi3.setBackgroundResource(R.drawable.shape_unselected_blue_circle)
+                    }
+                    2->{
+                        indi1.setBackgroundResource(R.drawable.shape_unselected_blue_circle)
+                        indi2.setBackgroundResource(R.drawable.shape_unselected_blue_circle)
+                        indi3.setBackgroundResource(R.drawable.shape_selected_blue_circle)
+                    }
+                }
+                curIndi++
+            }
+        },0, 1000)
     }
 }
