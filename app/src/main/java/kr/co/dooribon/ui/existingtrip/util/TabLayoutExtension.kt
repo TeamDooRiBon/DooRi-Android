0e85f42a821470fe602a8db037a69ecc08988a65
SSong-develop <thdgnsrl123@naver.com>
2021-07-06 03:33:28 +0900

    [FEAT] : WishList Navigation setting

---
+++ app/src/main/java/kr/co/dooribon/ui/existingtrip/util/TabLayoutExtension.kt
@@ -0,0 +1,59 @@
+package kr.co.dooribon.ui.existingtrip.util
+
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.tabs.TabLayout
+import kr.co.dooribon.R
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.AccommodationFragment
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.AllWishListFragment
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.HotRestaurantFragment
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.TourismFragment
+
+
+fun TabLayout.initializeWishListTab(){
+    this.apply {
+        addTab(this.newTab().setText("전체"))
+        addTab(this.newTab().setText("관광"))
+        addTab(this.newTab().setText("숙소"))
+        addTab(this.newTab().setText("맛집"))
+    }
+}
+
+fun TabLayout.initializeWishListTabNavigation(fragmentManager: FragmentManager){
+    this.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+        override fun onTabSelected(tab: TabLayout.Tab) {
+            when(tab.position){
+                0 -> {
+                    fragmentManager.beginTransaction().replace(
+                        R.id.fcv_wish_list,
+                        AllWishListFragment()
+                    ).commit()
+                }
+                1 -> {
+                    fragmentManager.beginTransaction().replace(
+                        R.id.fcv_wish_list,
+                        TourismFragment()
+                    ).commit()
+                }
+                2 -> {
+                    fragmentManager.beginTransaction().replace(
+                        R.id.fcv_wish_list,
+                        AccommodationFragment()
+                    ).commit()
+                }
+                3 -> {
+                    fragmentManager.beginTransaction().replace(
+                        R.id.fcv_wish_list,
+                        HotRestaurantFragment()
+                    ).commit()
+                }
+            }
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {
+        }
+    })
+}
+
--- app/src/main/java/kr/co/dooribon/ui/existingtrip/wishlist/WishListFragment.kt
+++ app/src/main/java/kr/co/dooribon/ui/existingtrip/wishlist/WishListFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.tabs.TabLayout
 import kr.co.dooribon.R
+import kr.co.dooribon.databinding.FragmentWishListBinding
+import kr.co.dooribon.ui.existingtrip.util.initializeWishListTab
+import kr.co.dooribon.ui.existingtrip.util.initializeWishListTabNavigation
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.AccommodationFragment
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.AllWishListFragment
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.HotRestaurantFragment
+import kr.co.dooribon.ui.existingtrip.wishlist.fragment.TourismFragment
+import kr.co.dooribon.utils.AutoClearBinding
 
 class WishListFragment : Fragment() {
+
+    private var binding by AutoClearBinding<FragmentWishListBinding>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wish_list, container, false)
+    ): View = FragmentWishListBinding.inflate(layoutInflater,container,false).also { FragmentWishListBinding ->
+        binding = FragmentWishListBinding
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.tabWishList.initializeWishListTab()
+        binding.tabWishList.initializeWishListTabNavigation(childFragmentManager)
     }
 }
