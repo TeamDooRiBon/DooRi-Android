ddf597be0c01d7c6d4bed119da8b20ffa9ddf231
cyj2825 <cyj2825@naver.com>
2021-07-08 15:01:16 +0900

    [FEAT]#46 - 기능 추가

--- app/src/main/java/kr/co/dooribon/ui/existingtrip/schedule/ScheduleAddActivity.kt
+++ app/src/main/java/kr/co/dooribon/ui/existingtrip/schedule/ScheduleAddActivity.kt
@@ -1,20 +1,56 @@
 package kr.co.dooribon.ui.existingtrip.schedule
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kr.co.dooribon.R
 import kr.co.dooribon.databinding.ActivityScheduleAddBinding
 
 class ScheduleAddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScheduleAddBinding
-
+    private var isClickable = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScheduleAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        notAddClickListener()
+        scheduleAddBtnClickListener()
+        addBackBtnClickListener()
     }
+    private fun notAddClickListener(){
+        binding.ivScheduleNotadd.setOnClickListener{
+            if (isClickable) {
+                binding.ivScheduleNotadd.setImageResource(R.drawable.ic_btn_notadd_active)
+                binding.etScheduleAddLocation.apply{
+                    setText(null)
+                    isClickable = false
+                    isEnabled = false
+                    setBackgroundResource(R.drawable.bg_edit_gray)
+                    hint = ""
+                }
+                isClickable = false
+            }
+            else if (!isClickable){
+                binding.ivScheduleNotadd.setImageResource(R.drawable.ic_btn_notadd)
+                binding.etScheduleAddLocation.apply{
+                    isEnabled = true
+                    setBackgroundResource(R.drawable.bg_edit_text_gray)
+                }
+                isClickable = true
+            }
 
+        }
+    }
+    private fun scheduleAddBtnClickListener(){
+        binding.btnScheduleAdd.setOnClickListener{
+            finish()
+        }
+    }
+    private fun addBackBtnClickListener() {
+        binding.ivScheduleAddBack.setOnClickListener {
+            finish()
+        }
+    }
 }
