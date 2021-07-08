0cc9a8c00fdd18aaa0c1bf8cd5f610e3add33587
Lee WonJoong <realjoong@gmail.com>
2021-07-08 19:34:07 +0900

    [CHORE] #7 - 탭 레이아웃 이미지 변경 완료
    - 다른 탭 클릭하면 원래대로 다시 돌리는 것 구현해야 함.

--- app/src/main/java/kr/co/dooribon/ui/existingtrip/board/BoardFragment.kt
+++ app/src/main/java/kr/co/dooribon/ui/existingtrip/board/BoardFragment.kt
@@ -1,10 +1,17 @@
 package kr.co.dooribon.ui.existingtrip.board
 
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import kr.co.dooribon.R
 import kr.co.dooribon.databinding.FragmentBoardBinding
 import kr.co.dooribon.ui.existingtrip.board.extension.initializeBoardTabNavigation
 import kr.co.dooribon.ui.existingtrip.extension.initializeTab
@@ -24,6 +31,79 @@ class BoardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         configureTabNavigation()
+        setTabDesign()
+        setTabAttribute()
+        onTabClickListener()
+    }
+
+    private fun onTabClickListener() {
+        binding.tabBoard.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Log.e("position", tab?.position.toString())
+                when(tab?.position){
+                    0->{
+                        tab.customView!!.findViewById<ImageView>(R.id.iv_tab_img).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_icon_board_goal_active))
+                    }
+                    1->{
+                        tab.customView!!.findViewById<ImageView>(R.id.iv_tab_img).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_icon_board_aim_inactive))
+                    }
+                    2->{
+                        tab.customView!!.findViewById<ImageView>(R.id.iv_tab_img).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_icon_board_role_inactive))
+                    }
+                    3->{
+                        tab.customView!!.findViewById<ImageView>(R.id.iv_tab_img).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_icon_board_check_inactive))
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+
+    private fun setTabAttribute() {
+        binding.tabBoard.apply {
+            tabRippleColor = null // 탭 리플 효과 제거
+        }
+    }
+
+    private fun setTabDesign() {
+        val firstView = layoutInflater.inflate(R.layout.custom_tab_board, null)
+        val firstImg = firstView.findViewById<ImageView>(R.id.iv_tab_img)
+        val firstTxt = firstView.findViewById<TextView>(R.id.tv_tab_text)
+
+        val secondView = layoutInflater.inflate(R.layout.custom_tab_board, null)
+        val secondImg = secondView.findViewById<ImageView>(R.id.iv_tab_img)
+        val secondTxt = secondView.findViewById<TextView>(R.id.tv_tab_text)
+
+        val thirdView = layoutInflater.inflate(R.layout.custom_tab_board, null)
+        val thirdImg = thirdView.findViewById<ImageView>(R.id.iv_tab_img)
+        val thirdTxt = thirdView.findViewById<TextView>(R.id.tv_tab_text)
+
+        val fourthView = layoutInflater.inflate(R.layout.custom_tab_board, null)
+        val fourthImg = fourthView.findViewById<ImageView>(R.id.iv_tab_img)
+        val fourthTxt = fourthView.findViewById<TextView>(R.id.tv_tab_text)
+
+        firstImg?.setImageResource(R.drawable.ic_icon_board_goal_inactive)
+        firstTxt?.text = getString(R.string.travel_goal)
+        binding.tabBoard.getTabAt(0)!!.customView = firstView
+
+        secondImg?.setImageResource(R.drawable.ic_icon_board_aim_inactive)
+        secondTxt?.text = getString(R.string.must_know)
+        binding.tabBoard.getTabAt(1)!!.customView = secondView
+
+        thirdImg?.setImageResource(R.drawable.ic_icon_board_role_inactive)
+        thirdTxt?.text = getString(R.string.role_alloc)
+        binding.tabBoard.getTabAt(2)!!.customView = thirdView
+
+        fourthImg?.setImageResource(R.drawable.ic_icon_board_check_inactive)
+        fourthTxt?.text = getString(R.string.checklist)
+        binding.tabBoard.getTabAt(3)!!.customView = fourthView
     }
 
     private fun configureTabNavigation() {
