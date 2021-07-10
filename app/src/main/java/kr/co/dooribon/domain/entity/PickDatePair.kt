83abffa1f63e40838e396a77310c053e458c5342
SSong-develop <thdgnsrl123@naver.com>
2021-07-11 03:35:30 +0900

    [CHORE] : 데이터 클래스 주석

--- app/src/main/java/kr/co/dooribon/domain/entity/PickDatePair.kt
+++ app/src/main/java/kr/co/dooribon/domain/entity/PickDatePair.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
+// 선택된 2개의 데이터 , 시작 날짜 , 끝 날짜 2개를 담아줄 데이터 클래스
 @Parcelize
 data class PickDatePair(
     val startDate : String,
