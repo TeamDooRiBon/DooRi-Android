3b96d4668ecf2a66b2d6c8b9fc49afc9a5a3dd62
Lee WonJoong <realjoong@gmail.com>
2021-07-12 14:00:01 +0900

    [FEAT] #83 - 여행 추가 뷰 뒤로가기 이벤트 추가

--- app/src/main/java/kr/co/dooribon/ui/newtrip/add/AddTravelActivity.kt
+++ app/src/main/java/kr/co/dooribon/ui/newtrip/add/AddTravelActivity.kt
@@ -1,11 +1,17 @@
 package kr.co.dooribon.ui.newtrip.add
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kr.co.dooribon.R
 import kr.co.dooribon.databinding.ActivityNewTravelBinding
+import kr.co.dooribon.dialog.TripTendencyTestExitDialog
 import kr.co.dooribon.domain.entity.PickDatePair
 import kr.co.dooribon.ui.newtrip.TravelPlanDoneActivity
 import kr.co.dooribon.ui.newtrip.adapter.ImageData
@@ -71,5 +77,28 @@ class AddTravelActivity : AppCompatActivity() {
         binding.ivBack.setOnClickListener {
             finish()
         }
+    }
+
+    override fun onBackPressed() {
+        setQuestionDialog()
+    }
+
+    private fun setQuestionDialog(){
+        val bsDialog = Dialog(this)
+        val sheetView = LayoutInflater.from(this).inflate(
+            R.layout.dialog_trip_tendency_test_exit,
+            this.findViewById(R.id.cl_exit_dialog_root)
+        )
+        sheetView.findViewById<TextView>(R.id.tv_exit_sub_description).text = "지금까지의 수정 정보는 저장되지 않습니다."
+        sheetView.findViewById<TextView>(R.id.tv_exit_sub_description2).text = "수정을 취소하려면 오른쪽 버튼을 눌러주세요.ㅤㅤ"
+        sheetView.findViewById<Button>(R.id.btn_no_exit).setOnClickListener {
+            bsDialog.dismiss()
+        }
+        sheetView.findViewById<Button>(R.id.btn_exit).setOnClickListener {
+            bsDialog.dismiss()
+            finish()
+        }
+        bsDialog.setContentView(sheetView)
+        bsDialog.show()
     }
 }
