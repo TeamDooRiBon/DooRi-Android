f65b02e612f5a9c5583e4cc16818e0d70e60b27b
SSong-develop <thdgnsrl123@naver.com>
2021-07-13 13:07:18 +0900

    [FEAT] : 여행 일정 추가 API

--- app/src/main/java/kr/co/dooribon/api/remote/ScheduleAPI.kt
+++ app/src/main/java/kr/co/dooribon/api/remote/ScheduleAPI.kt
@@ -1,5 +1,41 @@
 package kr.co.dooribon.api.remote
 
-interface ScheduleAPI {
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+import java.util.*
 
+// 여행 일정 추가
+data class CreateScheduleReq(
+    val title : String,
+    val scheduleStateTime : Date,
+    val scheduleEndTime : Date,
+    val scheduleLocation : String,
+    val scheduleMemo : String
+)
+
+data class CreateScheduleRes(
+    val createScheduleRes : BaseResponse<CreateSchedule>
+)
+
+data class CreateSchedule(
+    @SerializedName("_id")
+    val scheduleId : String,
+    @SerializedName("startTime")
+    val scheduleStartTime : String,
+    @SerializedName("formatTime")
+    val scheduleFormatTime : String,
+    @SerializedName("title")
+    val scheduleTitle : String,
+    @SerializedName("memo")
+    val scheduleMemo : String
+)
+
+interface ScheduleAPI {
+    @POST("schedule/{groupId}")
+    fun createSchedule(
+        @Path("groupId") groupId : String,
+        @Body createScheduleReq: CreateScheduleReq
+    ) : CreateScheduleRes
 }
