cdc7f1f03f09e0507bfddc52149bbdb0cfcd486a
SSong-develop <thdgnsrl123@naver.com>
2021-07-15 22:10:10 +0900

    [FEAT] : 그룹 유저별 성향 볼 수 있음

--- app/src/main/java/kr/co/dooribon/ui/traveltendencyresult/TravelTendencyResultActivity.kt
+++ app/src/main/java/kr/co/dooribon/ui/traveltendencyresult/TravelTendencyResultActivity.kt
@@ -33,58 +33,13 @@ class TravelTendencyResultActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_travel_tendency_result)
         binding.lifecycleOwner = this
         binding.vm = viewModel
+        binding.activity = this
 
         observeTravelTendencyResult()
         debugSSong(intent.getStringExtra("travelTendencyResultImageUrl"))
 
         intent.getStringExtra("travelTendencyResultImageUrl")?.let {
             viewModel.initializeTravelTendencyResultImageUrl(it)
-        }
-
-        binding.btnBack.setOnClickListener {
-            finish()
-        }
-
-        binding.clSaveTravelTendencyResult.setOnClickListener {
-            val fileName = "${System.currentTimeMillis()}.jpg" // 사진 파일 이름
-            val travelTendencyResultBitmap =
-                binding.ivTravelTendencyResult.drawable.toBitmap() // 저장할 사진 bitmap
-            var outputStream: OutputStream? = null
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                // getting the contentResolver
-                this.contentResolver.also { resolver ->
-
-                    // Content resolver will process the contentValues
-                    val contentValues = ContentValues().apply {
-
-                        // putting the file information in content values
-                        put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-                        put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-                    }
-
-                    // Inserting the contentValues to contentResolver and getting the Uri
-                    val imageUri: Uri? =
-                        resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
-                    // Opening an outputStream with the Uri that we got
-                    outputStream = imageUri?.let { resolver.openOutputStream(it) }
-                }
-            } else {
-                // These for devices running on android < Q
-                // So I don't think an explanation in needed here
-                val imagesDir =
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                val image = File(imagesDir, fileName)
-                outputStream = FileOutputStream(image)
-            }
-
-            outputStream?.use {
-                travelTendencyResultBitmap.compress(Bitmap.CompressFormat.JPEG,100,it)
-                shortToast("저장 성공")
-                finish()
-            }
         }
     }
 
@@ -98,6 +53,48 @@ class TravelTendencyResultActivity : AppCompatActivity() {
                 .override(this.resources.displayMetrics.widthPixels, this.dpToPixel(1531))
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .into(binding.ivTravelTendencyResult)
+        }
+    }
+
+    fun downloadBitmapInGallery() {
+        val fileName = "${System.currentTimeMillis()}.jpg" // 사진 파일 이름
+        val travelTendencyResultBitmap =
+            binding.ivTravelTendencyResult.drawable.toBitmap() // 저장할 사진 bitmap
+        var outputStream: OutputStream? = null
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // getting the contentResolver
+            this.contentResolver.also { resolver ->
+
+                // Content resolver will process the contentValues
+                val contentValues = ContentValues().apply {
+
+                    // putting the file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                // Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                // Opening an outputStream with the Uri that we got
+                outputStream = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            // These for devices running on android < Q
+            // So I don't think an explanation in needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, fileName)
+            outputStream = FileOutputStream(image)
+        }
+
+        outputStream?.use {
+            travelTendencyResultBitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            shortToast("저장 성공")
+            finish()
         }
     }
 }
