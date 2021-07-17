50b60adcb2703f87d372b418e4db289b3976ee77
SSong-develop <thdgnsrl123@naver.com>
2021-07-17 17:34:03 +0900

    [FEAT] : 일단 lazy로 만듬

--- app/src/main/java/kr/co/dooribon/di/ViewModelModule.kt
+++ app/src/main/java/kr/co/dooribon/di/ViewModelModule.kt
@@ -3,10 +3,13 @@ package kr.co.dooribon.di
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider
 import kr.co.dooribon.application.MainApplication.Companion.repositoryModule
+import kr.co.dooribon.ui.existingtrip.tendency.viewmodel.MemberViewModel
 import kr.co.dooribon.ui.factory.*
 
 /**
  * ViewModelFactory를 주입시켜주는 Module
+ *
+ * TODO : Lazy로 만들어서 캐시할 수 있도록 해야할거 같음!!!
  */
 class ViewModelModule(private val application: Application) {
 
@@ -27,4 +30,24 @@ class ViewModelModule(private val application: Application) {
 
     fun provideParticipateGroupViewModelFactory(): ViewModelProvider.Factory =
         ParticipateGroupViewModelFactory(repositoryModule.participateGroupRepository)
+
+    val homeViewModelFactory by lazy {
+        HomeViewModelFactory(repositoryModule.homeRepository)
+    }
+
+    val tripTendencyViewModelFactory by lazy {
+        TripTendencyViewModelFactory(repositoryModule.tripTendencyRepository)
+    }
+
+    val memberViewModelFactory by lazy {
+        MemberViewModelFactory(repositoryModule.tripTendencyRepository)
+    }
+
+    val detailViewModelFactory by lazy {
+        DetailViewModelFactory(repositoryModule.tripTendencyRepository)
+    }
+
+    val participateGroupViewModelFactory by lazy {
+        ParticipateGroupViewModelFactory(repositoryModule.participateGroupRepository)
+    }
 }
