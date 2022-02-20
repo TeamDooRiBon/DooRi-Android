168d9f34743f475774071c53fcb37abb98d121e6
Lee WonJoong <realjoong@gmail.com>
2022-02-20 16:37:45 +0900

    [CHORE] #156 - 마이페이지 액티비티 초기화

---
+++ app/src/main/java/kr/co/dooribon/utils/base/BaseActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.dooribon.utils.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) :
+    AppCompatActivity() {
+    protected lateinit var binding: T
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+    }
+}
